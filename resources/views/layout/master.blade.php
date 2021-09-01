80320d16ccd04cbb29108a9fcb1cf4697daf0aab
Aymendz46 <stormaymen@gmail.com>
2021-09-01 23:36:53 +0100

    trying to fix index.css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,7 +8,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @yield('page-style')
-    <!--link rel="stylesheet" href="./css/index.css"-->
     <link rel="stylesheet" href="./css/navbar.css">
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/loading.css">
