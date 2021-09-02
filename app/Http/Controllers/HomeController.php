3afee0a51fa154ae0c6518fb78460421817b0992
Aymendz46 <stormaymen@gmail.com>
2021-09-02 20:40:33 +0100

    More fixes for the alerts not being centered correctly, and spacing for the enroll now button in the course page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,9 +19,9 @@ class HomeController extends Controller
         if (! Newsletter::isSubscribed($request->email)) 
         {
             Newsletter::subscribe($request->email);
-            return back()->with('successMail', 'You joined the troops! Check your email!');
+            return back()->with('successMail', 'You joined the troops! Check your email for your first letter from the camp :).');
         }
 
-        return back()->with('failureMail', 'You are already subbed.');
+        return back()->with('failureMail', 'Oops, you are already subbed!');
     }
 }
